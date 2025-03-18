2718e29a6bae00be3b4adbb8f99ade9da8d91615
hackerloum <69545190+hackerloum@users.noreply.github.com>
2025-03-19 01:24:08 +0300

    Create postcss.config.mjs

---
+++ frontend/postcss.config.mjs
@@ -0,0 +1,14 @@
+export default {
+  plugins: {
+    'tailwindcss': {},
+    'postcss-preset-env': {
+      stage: 3,
+      features: {
+        'nesting-rules': true
+      }
+    },
+    'autoprefixer': {
+      flexbox: 'no-2009'
+    }
+  }
+}
