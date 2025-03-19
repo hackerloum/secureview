7acb449d1a474572ec1256525da539a92f62618c
hackerloum <69545190+hackerloum@users.noreply.github.com>
2025-03-19 05:14:05 +0300

    Update page.tsx

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import supabase from '@/lib/supabase';
+import supabase from '../../lib/supabase';
 import { Line } from 'react-chartjs-2';
 import {
   Chart as ChartJS,
